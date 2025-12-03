c53fd3c3a889d495cb7a1191f55f530f68bab042
francescovitale <64461360+francescovitale@users.noreply.github.com>
2025-12-03 13:06:52 +0100

    Update online_adaptation.bat

--- online_adaptation.bat
+++ online_adaptation.bat
@@ -1,7 +1,3 @@
-:: Options:
-:: pd_variant=[im, imf25, imf50, imf75, imf99, ilp, ilp25, ilp50, ilp75, ilp99, alpha]
-:: clustering_technique=[kmeans, agglomerative, gmm]
-
 set use_baseline=%1
 set pd_variant=%2
 set clustering_technique=%3
